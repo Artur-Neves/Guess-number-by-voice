5cd86edd6adace607699022936914ae71b3fd224
Artur Linux Acer <nevesdev.ti@gmail.com>
2024-02-17 17:41:37 -0300

    function game over

--- script/speechRecognition.js
+++ script/speechRecognition.js
@@ -11,6 +11,7 @@ recognition.addEventListener("result", onSpeak);
 function onSpeak(event) {
   let guess = event.results[0][0].transcript;
   number.textContent = guess;
+  gameOver(guess);
   validateNumber(guess);
 }
 recognition.addEventListener("end", ()=> recognition.start());
